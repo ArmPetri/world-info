8ec1a8974db51186bcaefbfc35812d6a950c1f03
Armands Petrikas <armandspetrikas@gmail.com>
2022-05-05 21:32:56 +0300

    Map SVG Cleanup

--- src/index.js
+++ src/index.js
@@ -1,17 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: 'https://countries.trevorblades.com/',
+  cache: new InMemoryCache()
+});
+
+function Countries() {
+  const { loading, error, data } = useQuery(gql`
+    {
+      countries {
+        code
+        name
+      }
+    }
+  `);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+console.log(data.countries.map((country) => {
+  return country
+  }))  
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
+    <Countries></Countries>
     <App />
-  </React.StrictMode>
+  </ApolloProvider>
+  
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
